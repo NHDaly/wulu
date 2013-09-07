e6d16706ce49a4dc4fcb1d6533d1565bcc29f6bf
Nathan Daly <NHDaly@gmail.com>
2013-09-07 14:13:13 -0400

    updated test.php

--- webpages/html/test.php
+++ webpages/html/test.php
@@ -1,11 +1,13 @@
-<html>
+<?php
 
-	<head>
+    # spawning PHP process for text-to-audio conversion
+    #  -- make sure to spawn in seperate process not linked to current page.
+    $spawnString = 'bash -c "exec nohup php addEpToDb.php < /dev/null > /dev/null 2>&1 &"';
+    exec($spawnString);
+    exec($spawnString);
+    exec($spawnString);
+    exec($spawnString);
 
-	</head>
+    echo "HELLO, i'm finished. c:\n";
 
-	<body>
-		this is my body
-	</body>
-
-</html>
+?>
