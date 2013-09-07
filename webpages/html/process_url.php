6e65cbedd38857d291ae6757c605b3f2578106be
Yuma Uesaka <yumasax@umich.edu>
2013-09-07 01:33:42 -0400

    updated process_url php file so it invokes the python magic

--- webpages/html/process_url.php
+++ webpages/html/process_url.php
@@ -1,6 +1,12 @@
 <?php
-  echo "url : " . $_POST["url"];
-$content = file_get_contents('http://tts-api.com/tts.mp3?q=hello+nathan');
-$file = "sound.mp3";
-file_put_contents($file, $content);
+# echo "url : " . $_POST["url"];
+# $content = file_get_contents('http://tts-api.com/tts.mp3?q=hello+nathan');
+# $file = "sound.mp3";
+# file_put_contents($file, $content);
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+# Run python script.
+$url = $_POST["url"];
+$result = exec('python ../../scripts/python/getrssfeed.py ' . $url);
+echo $result;
 ?>
