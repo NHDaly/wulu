25660884d1460bbdb9d0f6e0c5985eb5310abb59
Ubuntu <ubuntu@ip-10-10-127-48.ec2.internal>
2013-09-08 04:39:33 +0000

    finished (hopefully?) the updatePodcast py

--- scripts/python/updatePodcastWithDatabase.py
+++ scripts/python/updatePodcastWithDatabase.py
@@ -5,7 +5,10 @@ from sys import argv
 webUrl = 'http://ec2-54-226-137-31.compute-1.amazonaws.com/podcasts'
 
 def updatePodcastXml(rss_url, title, audio_filename, pubDate):
-    with open(podcast_local_url, 'r') as xmlFile:
+   # load current xml file
+   xmlFileUrl = '/var/www/podcasts/' + rss_url +'/podcast.xml'
+   xmlString = ''
+   with open(xmlFileUrl, 'r') as xmlFile:
      xmlString=''.join([line for line in xmlFile])
 
      tree = ET.fromstring(xmlString)
@@ -21,19 +24,27 @@ def updatePodcastXml(rss_url, title, audio_filename, pubDate):
      #TODO Find full audio web url
      audio_web_url = webUrl + rss_url + audio_filename
 
-
      #TODO Find length of audio file -- should we do this in php when adding to database?
-     
-     it_enclosure = ET.SubElement(item, 'enclosure', {'length':"12321",
+     it_enclosure = ET.SubElement(item, 'enclosure', {'url':audio_web_url, 'length':"12321",
                                                       'type':'audio/x-mp3'})
-     it_enclosure.text = audio_web_url
-     
      #TODO guid
      
-     it_pubDate = ET.SubElement(item, 'pubDate')
-     it_pubDate.text = pubDate
+     if pubDate: 
+        it_pubDate = ET.SubElement(item, 'pubDate')
+        it_pubDate.text = pubDate
 
-    return json.dumps({'XML':ET.tostring(tree),'episodes':articleList})
+   
+  #overwrite old file
+   with open(xmlFileUrl, 'w') as xmlFile:
+     xmlFile.write(xmlString)
 
-
+if __name__ == '__main__':
+    rss_url = argv[1]
+    title = argv[2]
+    audio_filename = argv[3]
+    if len(argv) > 4:
+        pubDate = argv[4]
+    else:
+        pubDate = None
+    updatePodcastXml(rss_url, title, audio_filename, pubDate)
 
