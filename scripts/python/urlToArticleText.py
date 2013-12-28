9b9154efb918f64eb8c0b46fd7bcfb2e710503d7
server <server@server.daly.private>
2013-12-28 00:50:28 -0500

    added a fix to the article text extraction to fix pages with cookies like NYTimes.

--- scripts/python/urlToArticleText.py
+++ scripts/python/urlToArticleText.py
@@ -8,8 +8,23 @@ g = Goose()
 
 # NOTE, on error this will return ''. Caller should check for this
 # and not try to text-to-speech it.
-cleanText=g.extract(argv[1]).cleaned_text
+url = argv[1] 
+cleanText=g.extract(url).cleaned_text
 cleanText=cleanText.strip(' \t\n\r')
+
+# Some websites need cookies to read them (eg NYTimes).
+# If reading fails, try again with cookies
+if (cleanText == ''):
+    print "EMPTY CLEAN TEXT"
+    import urllib2
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    response = opener.open(url)
+    
+    raw_html = response.read()
+    a = g.extract(raw_html=raw_html)
+    cleanText = a.cleaned_text
+
+
 with open(site_dir+'/podcasts/textout.txt', 'w') as argsoutFile:
     argsoutFile.write(cleanText.encode('utf8'))
 print json.dumps(cleanText)
